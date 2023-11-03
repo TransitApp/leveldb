4c9c8fc80ea7a9245e60b187fd031435ffef7a19
Felix Lapalme <lapfelix@users.noreply.github.com>
2023-11-03 01:32:14 -0400

    Try a more targeted warning suppression flag

--- Package.swift
+++ Package.swift
@@ -65,7 +65,7 @@ let package = Package(
         .define("HAVE_FULLFSYNC", to: "1"),
         .headerSearchPath("./"),
         .headerSearchPath("include/"),
-        .unsafeFlags(["-w"]),
+        .unsafeFlags(["-Wno-conversion"]),
       ]
     ),
   ],
